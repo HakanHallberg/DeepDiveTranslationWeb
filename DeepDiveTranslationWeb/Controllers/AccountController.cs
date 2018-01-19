b2ff20a8e4ba37aa3e9db55798a452e2610de5af
Håkan Hallberg <hakan.hallberg.spo16@ecusolna.onmicrosoft.com>
2018-01-19 10:08:58 +0100

    made small changes to var user so now it works

--- DeepDiveTranslationWeb/Controllers/AccountController.cs
+++ DeepDiveTranslationWeb/Controllers/AccountController.cs
@@ -66,7 +66,7 @@ namespace DeepDiveTranslationWeb.Controllers
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    var user = await _userManager.GetUserAsync(User);
+                    var user = await _userManager.FindByEmailAsync(model.Email);
                     var code = user == null || string.IsNullOrEmpty(user.CultureCode)
                         ? "en-US" : user.CultureCode;
                     var key = CookieRequestCultureProvider.DefaultCookieName;
